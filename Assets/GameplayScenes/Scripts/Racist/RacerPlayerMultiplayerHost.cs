c36fbfda7f1119a2da1fea736d263ab93d2b9707
szobek69420 <szobek.adam@gmail.com>
2025-05-15 22:36:36 +0200

    client side interpolation works

--- Assets/GameplayScenes/Scripts/Racist/RacerPlayerMultiplayerHost.cs
+++ Assets/GameplayScenes/Scripts/Racist/RacerPlayerMultiplayerHost.cs
@@ -8,6 +8,9 @@ public class RacerPlayerMultiplayerHost : RacerBase
     [SerializeField] private IngameCarController carController;
     [SerializeField] private Vector3 cameraOffset;
 
+    private Vector3 previousCameraOffset = Vector3.zero;
+    private Quaternion previousCameraRotation = Quaternion.identity;
+
     protected override void RacerUpdate()
     {
         if (gameManager?.State != GameManagerBase.GameState.RACE)
@@ -16,11 +19,12 @@ public class RacerPlayerMultiplayerHost : RacerBase
         ApplyBrakes();
         ApplyGas();
         ApplySteering();
+        UpdateCameraPosition();
     }
 
     protected override void RacerFixedUpdate()
     {
-        UpdateCameraPosition();
+        
     }
 
     protected override void RacerOnFinish()
@@ -44,46 +48,39 @@ public class RacerPlayerMultiplayerHost : RacerBase
 
     private void UpdateCameraPosition()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+        Rigidbody rb =GetComponent<Rigidbody>();
 
-        Vector3 position = Vector3.zero;
-        Quaternion rotation = Quaternion.identity;
+        Vector3 velocity = rb==null? Vector3.zero : rb.velocity;
+        Vector3 horizontalVelocity = new Vector3(velocity.x, 0.0f, velocity.z);
+        Vector3 offset;
+        Quaternion rotation;
 
-        if (rb == null || rb.velocity.magnitude < 1.0f)
+        if (horizontalVelocity.magnitude < 1.0f)
         {
-            position =
-                transform.position +
+            offset =
                 cameraOffset.z * transform.forward +
                 cameraOffset.x * transform.right +
                 cameraOffset.y * Vector3.up;
-            rotation = Quaternion.LookRotation(transform.position + transform.up - position);
+            rotation = Quaternion.LookRotation(transform.up - offset);
         }
         else
         {
-            Vector3 forward = rb.velocity;
-            forward.y = 0.0f;
-            forward = Vector3.Normalize(forward);
+            Vector3 forward = horizontalVelocity.normalized;
             Vector3 right = Vector3.Normalize(Vector3.Cross(Vector3.up, forward));
 
-            position =
-               transform.position +
+            float speedCoeff = 1.0f + 0.01f * horizontalVelocity.magnitude;
+            offset =
                cameraOffset.z * forward +
                cameraOffset.x * right +
                cameraOffset.y * Vector3.up;
-            rotation = Quaternion.LookRotation(transform.position + transform.up - position);
+            offset = new Vector3(speedCoeff * offset.x, offset.y, speedCoeff * offset.z);
+            rotation = Quaternion.LookRotation(transform.up - offset);
         }
 
+        previousCameraOffset = Vector3.Lerp(previousCameraOffset, offset, 0.2f);
+        previousCameraRotation = Quaternion.Lerp(previousCameraRotation, rotation, 0.2f);
 
-        Camera.main.transform.position = Vector3.Lerp(
-                    Camera.main.transform.position,
-                    position,
-                    0.2f
-                    );
-
-        Camera.main.transform.rotation = Quaternion.Lerp(
-            Camera.main.transform.rotation,
-            rotation,
-            0.2f
-            );
+        Camera.main.transform.position = transform.position + previousCameraOffset;
+        Camera.main.transform.rotation = previousCameraRotation;
     }
 }
